b3fed18db5a9c3b5a5655b8f0f54db662f2e9341
1shan-pixel <ispanta03@gmail.com>
2024-04-17 17:20:25 +0545

    insertion

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,29 +1,19 @@
-# The ideas is sort a list like [4,2,3,1] -> [1,2,3,4]
+#using insertion sort to sort a list 
 
-#Let's use insertion sort this time to sort the list
-
-#Insertion sort is a simple sorting algorithm that works the way we sort playing cards in our hands.
 
 def insertion_sort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
         j = i-1
-        while j>=0 and key<arr[j]:
+        while j>=0 and key <arr[j]:
             arr[j+1] = arr[j]
-            j -= 1 
+            j = j  -1 # dont forget to put such stopping conditions. 
         arr[j+1] = key
-    
+    return arr 
+
+# Example usage
+arr = [6,5,4,3,2,1]
+sorted_arr = insertion_sort(arr)
+print(sorted_arr)
 
 
-
-            
-    return arr
-
-       
-
-
-
-array = [5,3,7,2,1]
-
-array = insertion_sort(array)
-print(array)
